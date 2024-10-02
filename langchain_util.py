9a345235fcda6cda5a5bf65b2542bdf94e0d6ea4
ruichaozhg <65928844+ruichaozhg@users.noreply.github.com>
2024-10-02 09:39:47 +0000

    add memory

--- langchain_util.py
+++ langchain_util.py
@@ -2,6 +2,7 @@ from langchain_community.document_loaders import PyPDFLoader
 
 from langchain_core.prompts import (
     ChatPromptTemplate,
+    MessagesPlaceholder,
 )
 from langchain_community.embeddings import HuggingFaceEmbeddings
 import faiss
@@ -9,9 +10,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain_community.vectorstores import FAISS
 from uuid import uuid4
-from langchain.chains import create_retrieval_chain
+from langchain.chains import (ConversationalRetrievalChain,create_retrieval_chain,create_history_aware_retriever)
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
 import tempfile
 import os
 
@@ -66,7 +66,8 @@ def get_pdf_text(pdf_docs):
     # print(docs)
     return  docs
 
-def process_pdf(llm,pdf_doc,question,input_system_prompt):
+
+def process_pdf(pdf_doc):
     # loader = PyPDFLoader(file_path)
     # docs = uploaded_file.getvalue().decode("utf-8")
     # print(pdf_doc)
@@ -97,24 +98,87 @@ def process_pdf(llm,pdf_doc,question,input_system_prompt):
     vector_store.add_documents(documents=docs, ids=uuids)
 
     retriever = vector_store.as_retriever()
-    system_prompt = (
-        "You are an assistant for question-answering tasks. "
-        "Use the following pieces of retrieved context to answer "
-        "the question. If you don't know the answer, say that you "
-        "don't know. Use three sentences maximum and keep the "
-        "answer concise."
-        "\n\n"
-        "{context}"
-    )
-    # print(input_system_prompt == "")
-    final_system_prompt = input_system_prompt if input_system_prompt != "" else system_prompt
-    prompt = ChatPromptTemplate.from_messages(
+    return  retriever
+
+    ### version 1
+    # system_prompt = (
+    #     "You are an assistant for question-answering tasks. "
+    #     "Use the following pieces of retrieved context to answer "
+    #     "the question. If you don't know the answer, say that you "
+    #     "don't know. Use three sentences maximum and keep the "
+    #     "answer concise."
+    #     "\n\n"
+    #     "{context}"
+    # )
+    # # print(input_system_prompt == "")
+    # final_system_prompt = input_system_prompt if input_system_prompt != "" else system_prompt
+    # prompt = ChatPromptTemplate.from_messages(
+    #     [
+    #         ("system", final_system_prompt),
+    #         ("human", "{input}"),
+    #     ]
+    # )
+    # question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    # rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+    # results = rag_chain.invoke({"input": question})
+
+    ## version2
+
+def get_rag_chain(llm,retriever):
+
+
+
+    qa_system_prompt = """You are an assistant for question-answering tasks. \
+    Use the following pieces of retrieved context to answer the question. \
+    If you don't know the answer, just say that you don't know. \
+    Use three sentences maximum and keep the answer concise.\
+
+    {context}"""
+    qa_prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", final_system_prompt),
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("chat_history"),
             ("human", "{input}"),
         ]
     )
-    question_answer_chain = create_stuff_documents_chain(llm, prompt)
-    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
-    results = rag_chain.invoke({"input": question})
-    return results["answer"]
+
+
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+
+    # template = """Answer the question in your own words as truthfully as possible from the context given to you.
+    #     If you do not know the answer to the question, simply respond with "I don't know. Can you ask another question".
+    #     If questions are asked where there is no relevant context available, simply respond with "I don't know. Please ask a question relevant to the documents"
+    #     Context: {context}
+
+    #     {chat_history}
+    #     user: {question}
+    #     assistant: """
+
+    # prompt = PromptTemplate(
+    #     input_variables=["context", "chat_history", "question"], template=template
+    # )
+
+    # # Create the custom chain
+    # chain = ConversationalRetrievalChain.from_llm(
+    #     llm=llm, retriever=retriever,
+    #     get_chat_history=get_chat_history, return_source_documents=True,
+    #     combine_docs_chain_kwargs={'prompt': prompt})
+    # results = chain.invoke()
+    return rag_chain
--- main.py
+++ main.py
@@ -7,51 +7,116 @@ from langchain_community.document_loaders import WebBaseLoader
 from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
-from langchain_util import process_pdf
+from langchain_util import process_pdf,get_rag_chain
+from typing import Generator
 
+
+def generate_chat_responses(chat_completion) -> Generator[str, None, None]:
+    """Yield chat response content from the Groq API response."""
+    for chunk in chat_completion:
+        if chunk.choices[0].delta.content:
+            yield chunk.choices[0].delta.content
+def get_chat_history():
+    messages=[
+            {
+                "role": m["role"],
+                "content": m["content"]
+            }
+            for m in st.session_state.messages
+        ]
+    return messages
 def create_streamlit_app(llm, portfolio, clean_text):
-    st.title("Email Generator")
+    #st.title("Email Generator")
     #url_input = st.text_input("Enter a URL:", value="https://jobs.nike.com/job/R-33460")
-    url_input = st.text_input("Paste your link here:",value = "https://jobs.nike.com/job/R-35349" )
+    #url_input = st.text_input("Paste your link here:",value = "https://jobs.nike.com/job/R-35349" )
     # location_input = st.text_input("customer location: for example EU")
     # data_collection_input = st.multiselect('what data users collect', ["Log and Usage Data","Device Data","Location Data"])
     # user_control_right_input = st.text_input("enter your user control right here")
     # company_sharing_level_input = st.selectbox('how the company uses those data ', ["All parties","Limited third parties"])
     # default_link = None
-    submit_button = st.button("Email Generator Submit")
+    #submit_button = st.button("Email Generator Submit")
 
-    st.title("Check sidebar and upload template for Policy Generator")
+    # st.title("Check sidebar and upload template for Policy Generator")
 
     with st.sidebar:
         st.title("Upload your PDF files here:")
         pdf_doc = st.file_uploader("You may upload multiple files. Click on the Submit & Process Button", accept_multiple_files=False)
         if st.button("Submit & Process"):
+            st.session_state.retriever = process_pdf(pdf_doc)
+
             with st.spinner("Processing..."):
                 # docs = get_pdf_text(pdf_docs)
                 # text_chunks = get_text_chunks(docs)
                 # get_vector_store(text_chunks,GOOGLEPALM_API_KEY)
                 st.success("Done")
 
+    # version1
     # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not pdf_doc,
-    )
+    # question = st.text_area(
+    #     "Now ask a question about the document!",
+    #     placeholder="Can you give me a short summary?",
+    #     disabled=not pdf_doc,
+    # )
+    # 
+    # system_prompt = st.text_area(
+    #     "(Optional) Input your experiment system prompt",
+    #     value = "You are an assistant for question-answering tasks. "
+    #     "Use the following pieces of retrieved context to answer "
+    #     "the question. If you don't know the answer, say that you "
+    #     "don't know. Use three sentences maximum and keep the "
+    #     "answer concise."
+    #     "\n\n"
+    #     "{context}",
+    #     disabled=not pdf_doc,
+    # )
 
-    system_prompt = st.text_area(
-        "(Optional) Input your experiment system prompt",
-        value = "You are an assistant for question-answering tasks. "
-        "Use the following pieces of retrieved context to answer "
-        "the question. If you don't know the answer, say that you "
-        "don't know. Use three sentences maximum and keep the "
-        "answer concise."
-        "\n\n"
-        "{context}",
-        disabled=not pdf_doc,
-    )
-    if pdf_doc and question:
-        ans = process_pdf(llm.llm,pdf_doc,question,system_prompt)
+
+
+    # version2
+
+
+    if prompt := st.chat_input("Ask the question about the doc..."):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        with st.chat_message("user", avatar='😃'):
+            st.markdown(prompt)
+
+        # Fetch response from Groq API
+        try:
+
+            rag_chain = get_rag_chain(llm.llm, st.session_state.retriever)
+            result = rag_chain.invoke({"input": prompt, "chat_history": get_chat_history()})
+            chat_completion = result["answer"]
+            # Use the generator function with st.write_stream
+            with st.chat_message("assistant", avatar="📖"):
+                # chat_responses_generator = generate_chat_responses(chat_completion)
+                # full_response = st.write_stream(chat_responses_generator)
+                st.write(chat_completion)
+                full_response = chat_completion
+                # print("full_response",full_response)
+
+        except Exception as e:
+            st.error(e, icon="🚨")
+
+        # Append the full response to session_state.messages
+        if isinstance(full_response, str):
+            st.session_state.messages.append(
+                {"role": "assistant", "content": full_response})
+
+        else:
+            # Handle the case where full_response is not a string
+            combined_response = "\n".join(str(item) for item in full_response)
+            st.session_state.messages.append(
+                {"role": "assistant", "content": combined_response})
+
+
+
+
+
+
+    # if pdf_doc and question:
+    #     ans = process_pdf(llm.llm,pdf_doc,question,system_prompt)
+    #     st.write(ans)
 
         # Process the uploaded file and question.
         # document = uploaded_file.read().decode()
@@ -71,26 +136,37 @@ def create_streamlit_app(llm, portfolio, clean_text):
 
         # Stream the response to the app using `st.write_stream`.
         # print(ans)
-        st.write(ans)
-    if submit_button:
-        try:
-            loader = WebBaseLoader([url_input])
-            data = clean_text(loader.load().pop().page_content)
-            portfolio.load_portfolio()
-            jobs = llm.extract_jobs(data)
-            for job in jobs:
-                skills = job.get('skills', [])
-                links = portfolio.query_links(skills)
-                email = llm.write_mail(job, links)
-                st.code(email, language='markdown')
-        except Exception as e:
-            st.error(f"An Error Occurred: {e}")
+    # if submit_button:
+    #     try:
+    #         loader = WebBaseLoader([url_input])
+    #         data = clean_text(loader.load().pop().page_content)
+    #         portfolio.load_portfolio()
+    #         jobs = llm.extract_jobs(data)
+    #         for job in jobs:
+    #             skills = job.get('skills', [])
+    #             links = portfolio.query_links(skills)
+    #             email = llm.write_mail(job, links)
+    #             st.code(email, language='markdown')
+    #     except Exception as e:
+    #         st.error(f"An Error Occurred: {e}")
 
 
 if __name__ == "__main__":
     chain = Chain()
     portfolio = Portfolio()
     st.set_page_config(layout="wide", page_title="Policy Generator", page_icon="📧")
+
+    ## version2
+    # Initialize chat history and selected model
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+        st.session_state.retriever = None
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        avatar = '🧽️' if message["role"] == "assistant" else '😃'
+        with st.chat_message(message["role"], avatar=avatar):
+            st.markdown(message["content"])
+
     create_streamlit_app(chain, portfolio, clean_text)
 
 
