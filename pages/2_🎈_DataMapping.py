3fa91dfa9cba383ff9889f9eb8f244a099bd9fb0
ruichaozhg <65928844+ruichaozhg@users.noreply.github.com>
2024-10-09 08:05:11 +0000

    Add data mapping page

---
+++ pages/2_🎈_DataMapping.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title="DataMapping", page_icon="📊")
+
+st.markdown("# DataMapping")
+st.sidebar.header("DataMapping")
+
+
+st.markdown("Contact for Demo https://calendly.com/clare_hsu/30min")
